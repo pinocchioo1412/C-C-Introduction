dbca77e44a6414faec390f4e8354a91d5aa3f359
leduclong508-cpu <leduclong508@gmail.com>
2025-10-06 21:32:16 +0700

    oke anh

--- Duc Long/Test.cpp
+++ Duc Long/Test.cpp
@@ -1,3 +1,10 @@
-//
-// Created by admin on 10/5/2025.
-//
+#include<iostream>
+using namespace std;
+int main() {
+    string S;
+    getline(cin, S);
+    for (int i = 0; i < 3; i++) {
+        cout << S << endl;
+    }
+
+}
