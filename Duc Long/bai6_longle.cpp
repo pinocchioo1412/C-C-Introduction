0fd947990a4fb6630172f30bf6133ceab97c6a01
leduclong508-cpu <leduclong508@gmail.com>
2025-10-06 18:55:05 +0700

    oke anh

--- Duc Long/bai6_longle.cpp
+++ Duc Long/bai6_longle.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
-#include<cmath>
+#include <cmath>
 using namespace std;
+
 int main() {
     int n;
     int x = 0;
     cin >> n;
-    for (int i = n/3; i >= 0; i--) {
-        if (n - pow(2, i) > 0) {
-            x = i;
+    while (pow(2,x) <= n) {
+        x++;
+    }
+
+    for (int i = x-1; i >= 0; i--) {
+        if (n >= pow(2,i)) {
+            cout << "1";
+            n -= pow(2,i);
         }
-    cout << x << endl;
+        else {
+            cout << "0";
+        }
 
     }
 }
