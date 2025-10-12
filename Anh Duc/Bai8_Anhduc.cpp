aefa4031674accedd9755f45a098e240a160a660
Anh Duc <anhduc102.59@gmail.com>
2025-10-12 16:24:19 +0700

    sdfghjk

---
+++ Anh Duc/Bai8_Anhduc.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int a0 = 0;
+    cout << a0 << " ";
+    int a1 = 1;
+    cout << a1 << " ";
+    int a2;
+    for(int i = 0; i <= n - 3; i++){
+        a2 = a0 + a1;
+        cout << a2 << " ";
+        a0 = a1;
+        a1 = a2;
+    }
+}
