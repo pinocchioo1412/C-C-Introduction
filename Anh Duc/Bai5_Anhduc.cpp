25617ecbedc4c9bfdd6dc68504468f68b04355a9
Anh Duc <anhduc102.59@gmail.com>
2025-10-12 16:21:39 +0700

    sdfghjk

---
+++ Anh Duc/Bai5_Anhduc.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int a =0;
+    if(n <= 1){
+        cout << "Hay nhap so khac >= 2";
+    }
+    if(n >= 2){
+        for(int i = 2; i < n; i++){
+            if(n % i == 0){
+                a+=i;
+            }
+        }
+    }
+    if(a == 0){
+        cout << "Yes";
+    }
+    else {
+        cout << "No";
+    }
+}
