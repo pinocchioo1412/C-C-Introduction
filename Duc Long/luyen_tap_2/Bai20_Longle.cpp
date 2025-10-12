b684524dc2ec9543b9e3909aeafd13bbdcaa19a2
leduclong508-cpu <leduclong508@gmail.com>
2025-10-12 22:17:01 +0700

    oke anh

--- Duc Long/luyen_tap_2/Bai20_Longle.cpp
+++ Duc Long/luyen_tap_2/Bai20_Longle.cpp
@@ -1,3 +1,23 @@
-//
-// Created by ThinkPad on 10/12/2025.
-//
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    string filename;
+    cin >> filename;
+
+    ifstream file(filename);
+    if (!file) {
+        cout << "Khong mo duoc file." << endl;
+        return 1;
+    }
+
+    int lineCount = 0;
+    string line;
+    while (getline(file, line))
+        lineCount++;
+
+    cout << "So dong = " << lineCount << endl;
+    file.close();
+    return 0;
+}
