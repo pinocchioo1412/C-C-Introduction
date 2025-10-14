fa2189271c36e0e233d39627511a730eaf64e314
leduclong508-cpu <leduclong508@gmail.com>
2025-10-15 00:22:24 +0700

    oke anh

--- Duc Long/luyen_tap_2/Bai14_Longle.cpp
+++ Duc Long/luyen_tap_2/Bai14_Longle.cpp
@@ -2,20 +2,28 @@
 using namespace std;
 
 int main() {
-    int n;
+    int n, b;
     cin >> n;
 
-    int a[1000];
+    int a[n];
     for (int i = 0; i < n; i++)
         cin >> a[i];
 
-    for (int i = 0; i < n - 1; i++)
-        for (int j = 0; j < n - i - 1; j++)
-            if (a[j] > a[j + 1])
-                swap(a[j], a[j + 1]);
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                b = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = b;
+            }
+        }
+    }
 
     for (int i = 0; i < n; i++)
         cout << a[i] << " ";
 
+
     return 0;
+
+
 }
