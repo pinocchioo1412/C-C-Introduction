917390bf0b0dcefd8a092d7eb77dd355deade378
leduclong508-cpu <leduclong508@gmail.com>
2025-10-15 23:03:24 +0700

    oke anh

--- Duc Long/luyen_tap_2/Bai11_Longle.cpp
+++ Duc Long/luyen_tap_2/Bai11_Longle.cpp
@@ -5,7 +5,7 @@ int main() {
     string s;
     cin >> s;
 
-    int hoa = 0, thuong = 0, so = 0;
+    int hoa = 0, thuong = 0, so = 0, ki_tu = 0;
     for (auto c : s) {
         if (c >= '0' && c <= '9') {
             so += 1;
@@ -16,8 +16,11 @@ int main() {
         else if (c >= 'A' && c <= 'Z') {
             hoa += 1;
         }
+        else ki_tu += 1;
     }
     cout << "Hoa = " << hoa << endl;
     cout << "Thuong = " << thuong << endl;
     cout << "So = " << so << endl;
+    cout << "Ki tu dac biet = " << ki_tu << endl;
+    return 0;
 }
