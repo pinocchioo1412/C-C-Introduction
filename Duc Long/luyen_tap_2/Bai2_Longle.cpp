d44bdebc6918264baf720cc9520714a5ef53b8ee
leduclong508-cpu <leduclong508@gmail.com>
2025-10-20 14:41:48 +0700

    oke anh

--- Duc Long/luyen_tap_2/Bai2_Longle.cpp
+++ Duc Long/luyen_tap_2/Bai2_Longle.cpp
@@ -6,7 +6,6 @@ int main() {
     int a, b;
     cin >> a >> b;
     double thuong = double(a) / b;
-    thuong = int(thuong * 100 + 0.5) / 100.0;
     cout << "Tong = " << a + b << endl;
     cout << "Hieu = " << a - b << endl;
     cout << "Tich = " << a * b << endl;
