e9157668d9951adc0f4e0f4c496307fb3405a46e
DUC DUNG <leeducdungx@gmail.com>
2025-12-09 22:59:23 +0700

    getgo

--- Duc Dung/programming/class/Bai4_trenlop.cpp
+++ Duc Dung/programming/class/Bai4_trenlop.cpp
@@ -58,3 +58,83 @@ int main() {
     cout << "Nhan hang so: (" << N.hoanh_do << ", " << N.tung_do << ", " << N.cao_do << ")" << endl;
     cout << a.Nhan_vo_huong(a, b) << endl;
 }
+
+//code dang cap
+// #include <iostream>
+// using namespace std;
+//
+// class Vector {
+// public:
+//     double hoanh_do, tung_do, cao_do;
+//     static int count;
+//     Vector(double g, double h, double k) {
+//         this-> hoanh_do = g;
+//         this-> tung_do = h;
+//         this-> cao_do = k;
+//         count ++;
+//
+//     }
+//     Vector() {
+//         hoanh_do = 0;
+//         tung_do = 0;
+//         cao_do = 0;
+//         count ++;
+//     }
+//
+//     Vector(const Vector &v) {
+//         hoanh_do = v.hoanh_do;
+//         tung_do = v.tung_do;
+//         cao_do = v.cao_do;
+//         count ++;
+//     }
+//     ~Vector() { count --; }
+//
+//     void sethoanh_do(double h) {this ->hoanh_do = h;}
+//     void settung_do(double t) {this ->tung_do = t;}
+//     void setcao_do(double c) {this -> cao_do = c;}
+//
+//     Vector Tong_vector(Vector b) {
+//         Vector c;
+//         c.hoanh_do = hoanh_do + b.hoanh_do;
+//         c.tung_do = tung_do + b.tung_do;
+//         c.cao_do = cao_do + b.cao_do;
+//         return c;
+//     }
+//     Vector Tru_vector(Vector b) {
+//         Vector c;
+//         c.hoanh_do = hoanh_do - b.hoanh_do;
+//         c.tung_do = tung_do - b.tung_do;
+//         c.cao_do = cao_do - b.cao_do;
+//         return c;
+//     }
+//     Vector Nhan_hang_so(double k) {
+//         Vector v;
+//         v.hoanh_do = hoanh_do*k ;
+//         v.tung_do = tung_do*k;
+//         v.cao_do = cao_do*k;
+//         return v;
+//     }
+//     double Nhan_vo_huong(Vector& b) {
+//         return hoanh_do*b.hoanh_do + tung_do*b.tung_do + cao_do*b.cao_do;
+//     }
+//
+//     void In() {
+//         cout << "(" << hoanh_do << ", " << tung_do << ", " << cao_do << ")" << endl;
+//     }
+// };
+//
+// int Vector:: count = 0;
+//
+// int main() {
+//     Vector A(2,4,6);
+//     Vector B(2,3,7);
+//     double k = 2;
+//     Vector T = A.Tong_vector(B);
+//     cout << "Tong: " ; T.In();
+//     Vector H = A.Tru_vector(B);
+//     cout << "Hieu: " ; H.In();
+//     Vector N = A.Nhan_hang_so(k);
+//     cout << "Nhan hang so " << k << " voi A" << " : " ; N.In();
+//     cout << A.Nhan_vo_huong(B) << endl;
+//     cout << "So luong vector: " << Vector::count << endl;
+// }
